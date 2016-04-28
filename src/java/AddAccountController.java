f2b3be5247f70b6ad0170fee3e69794a8318154c
ewh0005 <ewh0005@auburn.edu>
2016-04-28 15:48:21 -0500

    Filled in code for AddAccountController
    Added a constructor and added preliminary code for it, create(), and
    clickAccountButton()

--- src/java/AddAccountController.java
+++ src/java/AddAccountController.java
@@ -2,13 +2,26 @@ public class AddAccountController {
 
    private Form addAccountForm;
    private Notification notification;
+   //private Account user;
+   private TomcatFormBuilder addAcctFormBuilder;
+   private TomcatNotificationFactory notifyFactory;
    
-   public void create() {
+   public AddAccountController(TomcatFormBuilder formBuilder, TomCatNotificationFactory notifFact) {
+      addAccountForm = null;
+      notification = null;
+      addAcctFormBuilder = formBuilder;
+      notifyFactory = notifFact;
+   }
    
+   private Form create() {
+      addAcctFormBuilder.addField("Username");
+      addAcctFormBuilder.addField("Password");
+      addAcctFormBuilder.addField("ID Number");
+      //addAcctFormBuilder.addField("
    }
    
    public Form clickAccountButton() {
-      create();
+      addAccountForm = create();
       return addAccountForm;
    }
    
