b8c9972798722c99018758bd94c07a04628ecbbf
Jeromy Swann <JMS0085@auburn.edu>
2016-05-02 21:20:47 -0500

    Implement submitForm in AddItemController

--- com/veggie/src/java/AddItemController.java
+++ com/veggie/src/java/AddItemController.java
@@ -2,21 +2,29 @@ package com.veggie.src.java;
 
 import com.veggie.src.java.form.*;
 import com.veggie.src.java.notification.*;
+import com.veggie.src.java.database.*;
+import com.veggie.src.java.MediaItem;
+import com.veggie.src.java.MediaTitle;
+
+import java.util.List;
 
 public class AddItemController implements Controller {
 
 	//Instance Variables
 	private Notification notification;
 	private Form addItemForm;
+	private TitleDatabaseManager manager;
+	private int titleStatus;
 	
 	//Methods
 	public AddItemController() {
 		AbstractFormBuilder builder = AbstractFormBuilderFactory.getInstance().createFormBuilder();
+		builder.addField("ISBN");
 		builder.addField("Title");
 		builder.addField("Author");
-		builder.addField("ISBN");
 		builder.addField("Description");
 		addItemForm = builder.getResult();
+		manager = AbstractDatabaseManagerFactory.getInstance().createTitleDatabaseManager();
 	}
 	
 	public Form clickAddItemButton() {
@@ -24,11 +32,19 @@ public class AddItemController implements Controller {
 	}
 	
 	public Notification submitForm() {
-		return null;
+		List<String> formData = addItemForm.getData();
+		String isbn = formData.get(0);
+		titleStatus = manager.checkTitle(isbn, addItemForm.getFieldNames(), formData);
+		if (titleStatus == MediaTitle.INVALID_TITLE) {
+			
+		} else {
+		
+		}
+		return notification;
 	}
 	
 	public void respondToNotification() {
-	
+		
 	}
 
 }
