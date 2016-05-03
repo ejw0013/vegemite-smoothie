4710aa8f02177327e28427ba3632db05fea112ac
Mitch Price <mcprice30@gmail.com>
2016-05-02 21:17:41 -0500

    Work on console

--- com/veggie/src/java/console/Console.java
+++ com/veggie/src/java/console/Console.java
@@ -1,5 +1,10 @@
 package com.veggie.src.java.console;
 
-public class Console {
+import com.veggie.src.java.notification.Notification;
+import com.veggie.src.java.form.Form;
 
+
+public interface Console {
+    public void renderNotification(Notification notification);
+    public void renderForm(Form form);
 }
