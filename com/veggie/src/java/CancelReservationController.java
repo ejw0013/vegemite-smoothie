b3b5e72dbac0997a55b894e8e32d1ebe6fad4fa7
Erich Wu <johnydaydream@gmail.com>
2016-05-02 20:17:12 -0500

    Add controller thing

--- com/veggie/src/java/CancelReservationController.java
+++ com/veggie/src/java/CancelReservationController.java
@@ -3,20 +3,41 @@ package com.veggie.src.java;
 import com.veggie.src.java.form.Form;
 import com.veggie.src.java.form.AbstractFormBuilder;
 import com.veggie.src.java.form.AbstractFormBuilderFactory;
+import com.veggie.src.java.database.AbstractDatabaseManagerFactory;
+import com.veggie.src.java.database.PrototypeAccountDatabaseManager;
 
 public class CancelReservationController implements Controller
 {
-  private AbstractFormBuilder fb;
-  private Form f;
+  private AbstractFormBuilder formBuilder;
+  private Form form;
 
   public CancelReservationController()
   {
-    fb = AbstractFormBuilderFactory.getInstance().createFormBuilder();
+    formBuilder = AbstractFormBuilderFactory.getInstance().createFormBuilder();
+    builder.addField("User ID");
+    builder.addField("Item ISBN");
+    form = formBuilder.getResult();
   }
 
   public Form createForm()
   {
-    return f;
+    return form;
   }
 
+  public void submitForm()
+  {
+  	  addAccountForm = form;
+      List<String> formData = form.getData();
+      String ISBN;
+      int id;
+      try{
+      	id = Integer.parseInt(formData.get(0));
+      	ISBN = formData.get(1);
+      }catch(Exception e){
+      	System.out.println("Error: incorrect form data");
+      }
+      TransactionDatabaseManager transactionDBManager = AbstractFormBuilderFactory.getInstace().createTransactionDatabaseManager();
+      transactionDBManager.finalizeReservation(id, ISBN);
+      return notification;   //???????
+  }
 }
