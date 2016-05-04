53b287c075df0cce02ea368755810e6a3ccaf88d
Erich Wu <johnydaydream@gmail.com>
2016-05-04 02:01:13 -0500

    Finished RenewController, removed removeItemController from transactions

--- com/veggie/src/java/controllers/transaction/RenewController.java
+++ com/veggie/src/java/controllers/transaction/RenewController.java
@@ -9,6 +9,7 @@ import com.veggie.src.java.form.Form;
 import com.veggie.src.java.form.AbstractFormBuilder;
 import com.veggie.src.java.form.AbstractFormBuilderFactory;
 import com.veggie.src.java.notification.Notification;
+import com.veggie.src.java.notification.AbstractNotificationFactory;
 import com.veggie.src.java.database.AbstractDatabaseManagerFactory;
 import com.veggie.src.java.database.TransactionDatabaseManager;
 import com.veggie.src.java.database.AccountDatabaseManager;
@@ -18,44 +19,48 @@ import com.veggie.src.java.core.transaction.RentalTransaction;
 public class RenewController implements Controller
 {
   private AbstractFormBuilder formBuilder;
-  private Form form;
-  Notification notification;
+  private int itemid;
+  private int userid;
 
-  public RenewController()
+  public RenewController(){}
+
+  public Form activate()
   {
     formBuilder = AbstractFormBuilderFactory.getInstance().createFormBuilder();
     formBuilder.addField("User ID");
     formBuilder.addField("Item ID");
-    form = formBuilder.getResult();
+    return formBuilder.getResult();
   }
 
-  public Form activate()
+  public Notification submitForm(Form form)
   {
-    return form;
-  }
-
-  public Notification submitForm()
-  {
-     List<String> formData = form.getData();
-      int itemid;
-      int userid;
-      try{
-      	userid = Integer.parseInt(formData.get(0));
-      	itemid = Integer.parseInt(formData.get(1));
-         TransactionDatabaseManager transactionDBManager = AbstractDatabaseManagerFactory.getInstance().createTransactionDatabaseManager();
-      transactionDBManager.finalizeTransaction(itemid);
-         AccountDatabaseManager accountDBManager = AbstractDatabaseManagerFactory.getInstance().createAccountDatabaseManager();
-         PatronAccount patron;
-         patron = (PatronAccount)accountDBManager.getUser(userid);
-         ItemDatabaseManager itemDBManager = AbstractDatabaseManagerFactory.getInstance().createItemDatabaseManager();
-         MediaItem med = itemDBManager.getItem(itemid);
-        RentalTransaction rent = new RentalTransaction(patron, med, 0);
-        transactionDBManager.addTransaction(rent);
-      }catch(Exception e){
-      	System.out.println("Error: incorrect form data");
+    PatronAccount patron;
+    Notification notification;
+    MediaItem med;
+    List<String> formData = form.getData();
+    try{
+      userid = Integer.parseInt(formData.get(0));
+      itemid = Integer.parseInt(formData.get(1));
+      ItemDatabaseManager itemDBManager = AbstractDatabaseManagerFactory.getInstance().createItemDatabaseManager();
+      TransactionDatabaseManager transactionDBManager = AbstractDatabaseManagerFactory.getInstance().createTransactionDatabaseManager();
+      if(itemDBManager.getItem(itemid) == null){
+        return AbstractNotificationFactory.getInstance().createErrorNotification("Error item does not exist!"); 
       }
+      AccountDatabaseManager accountDBManager = AbstractDatabaseManagerFactory.getInstance().createAccountDatabaseManager();
+      patron = (PatronAccount)accountDBManager.getUser(userid);
+      if(patron == null){
+        return AbstractNotificationFactory.getInstance().createErrorNotification("Error patron does not exist! "); 
+      }
+      transactionDBManager.finalizeTransaction(itemid);
+      med = itemDBManager.getItem(itemid);
+      RentalTransaction rent = new RentalTransaction(patron, med, 0);
+      transactionDBManager.addTransaction(rent);
+      notification = AbstractNotificationFactory.getInstance().createSuccessNotification("Checkout Renewed!"); 
+    }catch(Exception e){
+      notification = AbstractNotificationFactory.getInstance().createErrorNotification("Form data is incorrect!"); 
+    }
 
-      return notification;   //???????
+    return notification;   //???????
 
   }
 
