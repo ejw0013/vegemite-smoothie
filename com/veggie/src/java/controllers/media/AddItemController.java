3e58e6a304252e1ca8638b2397fc656b08f8e233
Jeromy Swann <JMS0085@auburn.edu>
2016-05-03 22:20:18 -0500

    Finish AddItemController

--- com/veggie/src/java/controllers/media/AddItemController.java
+++ com/veggie/src/java/controllers/media/AddItemController.java
@@ -1,12 +1,14 @@
-package com.veggie.src.java.controllers.media;
+package com.veggie.src.java;
 
-import com.veggie.src.java.controllers.Controller;
 import com.veggie.src.java.form.*;
 import com.veggie.src.java.notification.*;
 import com.veggie.src.java.database.*;
+import com.veggie.src.java.core.*;
+import com.veggie.src.java.core.media.MediaItem;
 import com.veggie.src.java.core.media.MediaTitle;
 
 import java.util.List;
+import java.util.Set;
 
 public class AddItemController implements Controller {
 
@@ -15,7 +17,7 @@ public class AddItemController implements Controller {
 	private Form addItemForm;
 	private TitleDatabaseManager manager;
 	private int titleStatus;
-
+	
 	//Methods
 	public AddItemController() {
 		AbstractFormBuilder builder = AbstractFormBuilderFactory.getInstance().createFormBuilder();
@@ -26,25 +28,39 @@ public class AddItemController implements Controller {
 		addItemForm = builder.getResult();
 		manager = AbstractDatabaseManagerFactory.getInstance().createTitleDatabaseManager();
 	}
-
-	public Form activate() {
+	
+	public Form clickAddItemButton() {
 	  return addItemForm;
 	}
-
+	
 	public Notification submitForm() {
 		List<String> formData = addItemForm.getData();
-		String isbn = formData.get(0);
-		titleStatus = manager.checkTitle(isbn, addItemForm.getFieldNames(), formData);
+		titleStatus = manager.checkTitle(formData.get(0), addItemForm.getFieldNames(), formData);
 		if (titleStatus == MediaTitle.INVALID_TITLE) {
-			//TO DO
+			notification = AbstractNotificationFactory.getInstance().createErrorNotification("Invalid Title: Media title exists, but fields do not match");
 		} else {
-			//TO DO
+			notification = AbstractNotificationFactory.getInstance().createConfirmNotification("Confirm item addition?");
 		}
 		return notification;
 	}
-
+	
 	public void respondToNotification() {
-		//TO DO
+		if (notification instanceof AbstractErrorNotification) {
+			if (notification.ok()) {
+				List<String> formData = addItemForm.getData();
+				MediaTitle title = new MediaTitle(0, "", "", "", "");
+				if (titleStatus == MediaTitle.NEW_TITLE) {
+					title = new MediaTitle(0, formData.get(0), formData.get(1), formData.get(2), formData.get(3));
+					manager.addTitle(formData.get(0), title);
+				} else if (titleStatus == MediaTitle.EXISTING_TITLE) {
+					Set<MediaTitle> titleResults = manager.getMatches(addItemForm.getFieldNames(), formData);
+					for (MediaTitle t : titleResults) {
+						title = t;
+					}
+				}
+				MediaItem newItem = new MediaItem(0, title);
+			}
+		}
 	}
 
 }
