86db359523eb54893bd6d5425646dd0af6858c04
Mitch Price <mcprice30@gmail.com>
2016-05-04 04:33:46 -0500

    Update Controller interface

--- com/veggie/src/java/controllers/Controller.java
+++ com/veggie/src/java/controllers/Controller.java
@@ -1,8 +1,11 @@
 package com.veggie.src.java.controllers;
 
 import com.veggie.src.java.form.Form;
+import com.veggie.src.java.notification.Notification;
 
 public interface Controller
 {
     public Form activate();
+    public Notification submitForm(Form form);
+    public void respondToNotification(Notification notification);
 }
