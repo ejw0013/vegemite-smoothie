e0a5509f30a6f915b996f52b12d84cf1f207a6bd
Jeromy Swann <JMS0085@auburn.edu>
2016-05-04 01:39:34 -0500

    Implement RespondToRequestController

--- com/veggie/src/java/controllers/transaction/RespondToRequestController.java
+++ com/veggie/src/java/controllers/transaction/RespondToRequestController.java
@@ -2,21 +2,41 @@ package com.veggie.src.java.controllers.transaction;
 
 import java.util.*;
 
-import com.veggie.src.java.form.Form;
-import com.veggie.src.java.notification.Notification;
+import com.veggie.src.java.form.*;
+import com.veggie.src.java.notification.*;
+import com.veggie.src.java.database.*;
 
 public class RespondToRequestController {
 
 	//Instance Variables
-
+	private Notification notification;
+	private Form respondForm;
+	private TransactionDatabaseManager manager;
 
 	//Methods
+	public RespondToRequestController() {
+		AbstractFormBuilder builder = AbstractFormBuilderFactory.getInstance().createFormBuilder();
+		builder.addField("Request ID");
+		builder.addField("Response");
+		respondForm = builder.getResult();
+		manager = AbstractDatabaseManagerFactory.getInstance().createTransactionDatabaseManager();
+	}
+	
 	public Form activate() {
-		return null;
+		return respondForm;
+	}
+	
+	public Notification submitForm(Form form) {
+		respondForm = form;
+		notification = AbstractNotificationFactory.getInstance().createConfirmNotification("Proceed with request response?");
+		return notification;
 	}
 
  	public void respondToNotification(Notification notif) {
-    //TO DO
+    if (notif.ok()) {
+			List<String> formData = respondForm.getData();
+			manager.finalizeTransaction(Integer.parseInt(formData.get(0)));
+		}
   }
 
 }
