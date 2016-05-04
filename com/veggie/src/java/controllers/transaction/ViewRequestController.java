536b0b40c41216ade6f175c20327ca8f9e3aa057
Erich Wu <johnydaydream@gmail.com>
2016-05-04 03:50:21 -0500

    ViewRequestController

---
+++ com/veggie/src/java/controllers/transaction/ViewRequestController.java
@@ -0,0 +1,42 @@
+package com.veggie.src.java.controllers.transaction;
+
+import java.util.*;
+
+import com.veggie.src.java.form.*;
+import com.veggie.src.java.notification.*;
+import com.veggie.src.java.database.*;
+import com.veggie.src.java.controllers.Controller;
+import com.veggie.src.java.core.account.PatronAccount;
+import com.veggie.src.java.core.transaction.RequestTransaction;
+import com.veggie.src.java.core.Transaction;
+
+public class ViewRequestController implements Controller {
+
+	//Instance Variables
+	private Notification notification;
+	private Form respondForm;
+	private TransactionDatabaseManager manager;
+
+	//Methods
+	public ViewRequestController() {}
+
+	public Form activate() {
+		return null;
+	}
+
+	public Notification submitForm(Form form) {
+		StringBuilder sb = new StringBuilder();
+		TransactionDatabaseManager transactionDBManager = AbstractDatabaseManagerFactory.getInstance().createTransactionDatabaseManager();
+		List<Transaction> requestList = transactionDBManager.getRequests();
+		for(Transaction transaction : requestList){
+			RequestTransaction rtransaction = (RequestTransaction) transaction;
+			sb.append("Request ID: " + rtransaction.getId() + " Patron Requesting: " + rtransaction.getPatronAccount().getusername());
+			sb.append("Request description: " + rtransaction.getDescription() + "\n");
+		}
+		notification = AbstractNotificationFactory.getInstance().createSuccessNotification(sb.toString());
+		return notification;
+	}
+
+ 	public void respondToNotification(Notification notif) {}
+
+}
