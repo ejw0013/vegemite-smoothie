4f7bb45b76478ae22d7ffc867861d6c2db469224
Erich Wu <johnydaydream@gmail.com>
2016-05-04 02:12:10 -0500

    Updated ReturnController and RenewController

--- com/veggie/src/java/controllers/transaction/ReturnController.java
+++ com/veggie/src/java/controllers/transaction/ReturnController.java
@@ -6,40 +6,43 @@ import com.veggie.src.java.form.Form;
 import com.veggie.src.java.form.AbstractFormBuilder;
 import com.veggie.src.java.form.AbstractFormBuilderFactory;
 import com.veggie.src.java.notification.Notification;
+import com.veggie.src.java.notification.AbstractNotificationFactory;
 import com.veggie.src.java.database.AbstractDatabaseManagerFactory;
 import com.veggie.src.java.database.TransactionDatabaseManager;
+import com.veggie.src.java.database.ItemDatabaseManager;
 
 public class ReturnController implements Controller
 {
   private AbstractFormBuilder formBuilder;
-  private Form form;
-  Notification notification;
+  private int itemid;
 
-  public ReturnController()
-  {
-    formBuilder = AbstractFormBuilderFactory.getInstance().createFormBuilder();
-    formBuilder.addField("Item ID");
-    form = formBuilder.getResult();
-  }
+  public ReturnController(){}
 
   public Form activate()
   {
-    return form;
+    formBuilder = AbstractFormBuilderFactory.getInstance().createFormBuilder();
+    formBuilder.addField("Item ID");
+    return formBuilder.getResult();
   }
 
-  public Notification submitForm()
+  public Notification submitForm(Form form)
   {
-      List<String> formData = form.getData();
-      int itemid;
-      try{
-      	itemid = Integer.parseInt(formData.get(0));
-      	TransactionDatabaseManager transactionDBManager = AbstractDatabaseManagerFactory.getInstance().createTransactionDatabaseManager();
-      transactionDBManager.finalizeTransaction(itemid);
-      }catch(Exception e){
-      	System.out.println("Error: incorrect form data");
+    Notification notification;
+    List<String> formData = form.getData();
+    try{
+    	itemid = Integer.parseInt(formData.get(0));
+    	TransactionDatabaseManager transactionDBManager = AbstractDatabaseManagerFactory.getInstance().createTransactionDatabaseManager();
+      ItemDatabaseManager itemDBManager = AbstractDatabaseManagerFactory.getInstance().createItemDatabaseManager();
+      if(itemDBManager.getItem(itemid) == null){
+        return AbstractNotificationFactory.getInstance().createErrorNotification("Error item does not exist!");
       }
+      transactionDBManager.finalizeTransaction(itemid);
+      notification = AbstractNotificationFactory.getInstance().createSuccessNotification("Item sucessfully returned!");
+    }catch(Exception e){
+    	notification = AbstractNotificationFactory.getInstance().createErrorNotification("Error form data failure");
+    }
 
-      return notification;   //???????
+    return notification;   //???????
   }
 
   public void respondToNotification(Notification notif) {
