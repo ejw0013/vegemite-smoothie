4b6717217160f232464f7fe5d3c0ff8fb571caff
Erich Wu <johnydaydream@gmail.com>
2016-05-03 21:00:40 -0500

    Fixed typo in faculty account

--- com/veggie/src/java/entities/accounts/patrons/FacultyAccount.java
+++ com/veggie/src/java/entities/accounts/patrons/FacultyAccount.java
@@ -4,7 +4,7 @@ import com.veggie.src.java.entities.accounts.PatronAccount;
 
 public class FacultyAccount extends PatronAccount {
 
-	public final String type = "FacultytAccount";
+	public final String type = "FacultyAccount";
 	private int facultyId;
 
 	public FacultyAccount(final String username, final String contactInformation, final int id, final int facultyId, final String password){
