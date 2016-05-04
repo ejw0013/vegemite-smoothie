d5adfb983fdeac0ce8e8731fe7e193493f97c5d5
Jeromy Swann <JMS0085@auburn.edu>
2016-05-03 23:29:14 -0500

    Implement EditItemController

--- com/veggie/src/java/controllers/media/EditItemController.java
+++ com/veggie/src/java/controllers/media/EditItemController.java
@@ -4,21 +4,21 @@ import com.veggie.src.java.controllers.Controller;
 import com.veggie.src.java.form.*;
 import com.veggie.src.java.notification.*;
 import com.veggie.src.java.database.*;
+import com.veggie.src.java.core.media.MediaItem;
 
 import java.util.List;
 
 public class EditItemController implements Controller {
 
 	//Instance Variables
-	private int itemID;
 	private Notification notification;
 	private Form editItemForm;
 	private ItemDatabaseManager manager;
 
 	//Methods
-	public EditItemController(int id) {
-		itemID = id;
+	public EditItemController() {
 		AbstractFormBuilder builder = AbstractFormBuilderFactory.getInstance().createFormBuilder();
+		builder.addField("Item ID");
 		builder.addField("Status");
 		editItemForm = builder.getResult();
 		manager = AbstractDatabaseManagerFactory.getInstance().createItemDatabaseManager();
@@ -29,13 +29,26 @@ public class EditItemController implements Controller {
 	}
 
 	public Notification submitForm(Form form) {
-		List<String> formData = editItemForm.getData();
-		//TO DO
+		editItemForm = form;
+		notification = AbstractNotificationFactory.getInstance().createConfirmNotification("Proceed with edit?");
 		return notification;
 	}
 
 	public void respondToNotification(Notification notif) {
-		//TO DO
+		if (notif.ok()) {
+			List<String> formData = editItemForm.getData();
+			switch (Integer.parseInt(formData.get(1))) {
+				case MediaItem.AVAILABLE:
+					manager.markAvailable(Integer.parseInt(formData.get(0)));
+					break;
+				case MediaItem.UNAVAILABLE:
+					manager.markUnavailable(Integer.parseInt(formData.get(0)));
+					break;
+				case MediaItem.RESERVED:
+					manager.markReserved(Integer.parseInt(formData.get(0)));
+					break;
+			}
+		}
 	}
 
 }
