5a86f19a1a597c1d74d41b59c1627f9458a9a39b
ewh0005 <ewh0005@auburn.edu>
2016-05-03 21:29:36 -0500

    Filled in more of ReportController

--- com/veggie/src/java/controllers/account/ReportController.java
+++ com/veggie/src/java/controllers/account/ReportController.java
@@ -8,6 +8,10 @@ import com.veggie.src.java.form.AbstractFormBuilder;
 import com.veggie.src.java.form.AbstractFormBuilderFactory;
 import com.veggie.src.java.database.AbstractDatabaseManagerFactory;
 import com.veggie.src.java.database.AccountDatabaseManager;
+import com.veggie.src.java.core.account.EmployeeAccount;
+import com.veggie.src.java.core.account.patron.FacultyAccount;
+import com.veggie.src.java.core.account.patron.GraduateStudentAccount;
+import com.veggie.src.java.core.account.patron.UndergraduateStudentAccount;
 
 import java.util.List;
 
@@ -32,6 +36,8 @@ public class ReportController implements Controller {
       reportForm = form;
       List<String> formData = reportForm.getData();
       Account user = manager.getUser(Integer.parseInt(formData.get(0)));
+      String report = generateReport(user);
+      /*
       String report = "Account Report for: " + user.getusername() + "\n";
       String accountType = user.getType();
       report += "Account Type: " + accountType + "\n";
@@ -39,10 +45,50 @@ public class ReportController implements Controller {
       else report += "Account Status: Suspended\n";
       report += "Contact Information: " + user.getContactInformation() + "\n";
       report += "Library ID Number: " + user.getId() + "\n";
-      //if (accountType == "AssistantAccount" || accountType = "LibrarianAccount")
-         //report += "Employee ID Number: " + user.getEmployeeID() + "\n";
-      //if (accountType == "FacultyAccount") report += "Faculty ID Number: " + user.getFacultyID() + "\n";
-      
+      if (accountType.equals("AssistantAccount") || accountType.equals("LibrarianAccount")) {
+         EmployeeAccount employee = (EmployeeAccount) user;
+         report += "Employee ID Number: " + employee.getEmployeeId() + "\n";
+      }
+      if (accountType.equals("FacultyAccount")) {
+         FacultyAccount faculty = (FacultyAccount) user;
+         report += "Faculty ID Number: " + faculty.getFacultyId() + "\n";
+      }
+      if (accountType.equals("UndergraduateStudentAccount")) {
+         UndergraduateStudentAccount undergrad = (UndergraduateStudentAccount) user;
+         report += "Student ID Number: " + undergrad.getStudentId() + "\n";
+      }
+      if (accountType.equals("GraduateStudentAccount")) {
+         UndergraduateStudentAccount grad = (UndergraduateStudentAccount) user;
+         report += "Student ID Number: " + grad.getStudentId() + "\n";
+      }*/
       return notification;
+   }
+   
+   private String generateReport(Account user) {
+      String report = "Account Report for: " + user.getusername() + "\n";
+      String accountType = user.getType();
+      report += "Account Type: " + accountType + "\n";
+      if (user.isActive() == true) report += "Account Status: Active\n";
+      else report += "Account Status: Suspended\n";
+      report += "Contact Information: " + user.getContactInformation() + "\n";
+      report += "Library ID Number: " + user.getId() + "\n";
+      if (accountType.equals("AssistantAccount") || accountType.equals("LibrarianAccount")) {
+         EmployeeAccount employee = (EmployeeAccount) user;
+         report += "Employee ID Number: " + employee.getEmployeeId() + "\n";
+      }
+      if (accountType.equals("FacultyAccount")) {
+         FacultyAccount faculty = (FacultyAccount) user;
+         report += "Faculty ID Number: " + faculty.getFacultyId() + "\n";
+      }
+      if (accountType.equals("UndergraduateStudentAccount")) {
+         UndergraduateStudentAccount undergrad = (UndergraduateStudentAccount) user;
+         report += "Student ID Number: " + undergrad.getStudentId() + "\n";
+      }
+      if (accountType.equals("GraduateStudentAccount")) {
+         UndergraduateStudentAccount grad = (UndergraduateStudentAccount) user;
+         report += "Student ID Number: " + grad.getStudentId() + "\n";
+      }
+      report += "Current Fee Balance: " + user.getFees() + "\n";
+      return report;
    }
 }
