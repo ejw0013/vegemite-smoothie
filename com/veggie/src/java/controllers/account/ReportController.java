a78141e507ec2001a3859c0018e4496a965c6110
ewh0005 <ewh0005@auburn.edu>
2016-05-03 21:57:46 -0500

    Finished ReportController
    *except notifications

--- com/veggie/src/java/controllers/account/ReportController.java
+++ com/veggie/src/java/controllers/account/ReportController.java
@@ -9,9 +9,11 @@ import com.veggie.src.java.form.AbstractFormBuilderFactory;
 import com.veggie.src.java.database.AbstractDatabaseManagerFactory;
 import com.veggie.src.java.database.AccountDatabaseManager;
 import com.veggie.src.java.core.account.EmployeeAccount;
+import com.veggie.src.java.core.account.PatronAccount;
 import com.veggie.src.java.core.account.patron.FacultyAccount;
 import com.veggie.src.java.core.account.patron.GraduateStudentAccount;
 import com.veggie.src.java.core.account.patron.UndergraduateStudentAccount;
+import com.veggie.src.java.core.Transaction;
 
 import java.util.List;
 
@@ -37,31 +39,7 @@ public class ReportController implements Controller {
       List<String> formData = reportForm.getData();
       Account user = manager.getUser(Integer.parseInt(formData.get(0)));
       String report = generateReport(user);
-      /*
-      String report = "Account Report for: " + user.getusername() + "\n";
-      String accountType = user.getType();
-      report += "Account Type: " + accountType + "\n";
-      if (user.isActive() == true) report += "Account Status: Active\n";
-      else report += "Account Status: Suspended\n";
-      report += "Contact Information: " + user.getContactInformation() + "\n";
-      report += "Library ID Number: " + user.getId() + "\n";
-      if (accountType.equals("AssistantAccount") || accountType.equals("LibrarianAccount")) {
-         EmployeeAccount employee = (EmployeeAccount) user;
-         report += "Employee ID Number: " + employee.getEmployeeId() + "\n";
-      }
-      if (accountType.equals("FacultyAccount")) {
-         FacultyAccount faculty = (FacultyAccount) user;
-         report += "Faculty ID Number: " + faculty.getFacultyId() + "\n";
-      }
-      if (accountType.equals("UndergraduateStudentAccount")) {
-         UndergraduateStudentAccount undergrad = (UndergraduateStudentAccount) user;
-         report += "Student ID Number: " + undergrad.getStudentId() + "\n";
-      }
-      if (accountType.equals("GraduateStudentAccount")) {
-         UndergraduateStudentAccount grad = (UndergraduateStudentAccount) user;
-         report += "Student ID Number: " + grad.getStudentId() + "\n";
-      }*/
-      return notification;
+      return notification; //notifications
    }
    
    private String generateReport(Account user) {
@@ -89,6 +67,16 @@ public class ReportController implements Controller {
          report += "Student ID Number: " + grad.getStudentId() + "\n";
       }
       report += "Current Fee Balance: " + user.getFees() + "\n";
+      if (accountType.equals("UndergraduateStudentAccount") || accountType.equals("GraduateStudentAccount") || accountType.equals("FacultyAccount")) {
+         PatronAccount patron = (PatronAccount) user;
+         List<Transaction> transactions = patron.getTransactions();
+         int size = transactions.size();
+         report += "Transaction History:\n";
+         for(int i = 0; i < size; i++) {
+            Transaction tran = transactions.get(i);
+            report += tran.toString() + "\n";
+         }
+      }
       return report;
    }
 }
